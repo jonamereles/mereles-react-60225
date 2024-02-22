036339712e6c2a4580b96d6c44cde50b64aeb3cb
jonathan mereles <jonathan_mereles@hotmail.com>
2024-02-22 18:54:01 -0300

    modificado

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,15 +1,16 @@
 import '../Item/Item.css'
+
 export const Item = ({ product }) => {
     return (
         <div className=" max-w-xs mx-auto bg-gray-200 p-3 rounded-md shadow-md">
-            <img src={product.img} alt={`Imagen de ${product.title}`} />
-            <h2 className="text-lg font-semibold font-bold mb-2">{product.title} {product.marca} </h2>
+            <img src={`./data/img/${product.img}`} alt={`Imagen de ${product.title}`} />
+            <h2 className="text-lg font-semibold font-bold mb-2">{product.title} {product.brand} </h2>
             <p className="text-gray-700 mb-2 font-semibold">{product.description}</p>
             <p className="text-gray-700 mb-2">Stock: {product.stock}</p>
             <div className="flex justify-between items-center">
-                <span className="text-xl font-bold">U$D{product.price}</span>
+                <span className="text-xl font-bold">${product.price}</span>
                 <button className="bg-cyan-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none">
-                    Agregar al carrito
+                Agregar al carrito
                 </button>
             </div>
         </div>
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,19 @@
 import '../ItemListContainer/ItemListContainer.css'
+import { useState, useEffect } from "react"
+import { ItemList } from "../ItemList/ItemList"
 export const ItemListContainer = ({ greeting }) => {
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        fetch('./data/productos.json')
+            .then(response => response.json())
+            .then(prods => {
+                setProducts(prods)
+            })
+            .catch((error) => console.log(error))
+    }, [])
     return (
         <div>
-            <p>{greeting}</p>
+            <ItemList products={products} />
         </div>
     )
 }
-export default ItemListContainer
